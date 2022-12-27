f50862e73bb72d3895a9122c1cf60fcfb67aff9f
Владимир Ширина <64553688+Tionthe1@users.noreply.github.com>
2022-12-27 12:25:35 +0200

    Homework_04

--- Homework_04/js/script.js
+++ Homework_04/js/script.js
@@ -1,33 +1,9 @@
-/* !Функція повертає масив або інше значення, але не змінює його!
-У консоль/html виводите результат виклику функції:
-const pairs = getPairs(students);
-console.log(pairs);
-Перед виконанням цього домашнього завдання необхідно створити 3 масиви:
-const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
-const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і
-структури даних"];
-const marks = [4, 5, 5, 3, 4, 5]; */
-/* 
-1.Розділіть студентів на пари(хлопець + дівчина) для работи над проєктом. У вас
-повинен вийти вкладений масив з парами студентів: [["Олександр", "Олена"], [..],
-[...]];
-
-2.Зіставте пари з попереднього завдання та теми проєктів, над якими студенти
-будуть працювати. Повинен вийти вкладений масив виду: [["Олександр і Олена",
-"Теорія автоматів"], [...], [...]]
-
-3.Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...], [...]]
-
-4.Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт(тут функція буде
-нечистою, але не повинна мутувати массив): [["Олександр і Олена", "Теорія
-автоматів", 5], [...], [...]] */
-
 const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 
 const themes = [
   "Диференційне рівняння",
   "Теорія автоматів",
-  "Алгоритми і структури даних"
+  "Алгоритми і структури даних",
 ];
 const marks = [4, 5, 5, 3, 4, 5];
 
@@ -49,8 +25,55 @@ console.log(getPairs(students));
 будуть працювати. Повинен вийти вкладений масив виду: [["Олександр і Олена",
 "Теорія автоматів"], [...], [...]] */
 
-//pairs [[],[],[]], themes['','','']
+//pairs [[],[],[]], themes['','','']()
 
 //pairsThemes[['',''], ['',''], ['','']]
 
+const pairsThemes = getPairsThemes(pairs, themes);
 
+function getPairsThemes(pairs, themes) {
+  const arrPairs = pairs.slice();
+  let arrPairsThemes = [];
+  arrPairsThemes.push(
+    [arrPairs[0][0] + " і " + arrPairs[0][1] + ", " + themes[0]],
+    [arrPairs[1][0] + " і " + arrPairs[1][1] + ", " + themes[1]],
+    [arrPairs[2][0] + " і " + arrPairs[2][1] + ", " + themes[2]]
+  );
+  return arrPairsThemes;
+}
+console.log(getPairsThemes(pairs, themes));
+//console.log(students, themes, marks);
+
+//3.Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...], [...]]
+
+const studentsMarks = getStudentsMarks(students, marks);
+
+function getStudentsMarks(students, marks) {
+  let arrStudMarks = [];
+  arrStudMarks.push(
+    [students[0], marks[0]],
+    [students[1], marks[1]],
+    [students[2], marks[2]],
+    [students[3], marks[3]],
+    [students[4], marks[4]],
+    [students[5], marks[5]]
+  );
+  return arrStudMarks;
+}
+console.log(studentsMarks);
+
+/* 4.Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт(тут функція буде
+нечистою, але не повинна мутувати массив): [["Олександр і Олена", "Теорія
+автоматів", 5], [...], [...]] */
+
+const pairsMarks = getPairsMarks(pairsThemes, marks);
+
+function getPairsMarks(pairsThemes) {
+  let arrPairsMarks = [];
+  for (let i = 0; i < pairsThemes.length; i++) {
+    let randomNumber = Math.round(Math.random() * (5 - 1) + 1);
+    arrPairsMarks.push([...pairsThemes[i], randomNumber]);
+  }
+  return arrPairsMarks;
+}
+console.log(pairsMarks);
