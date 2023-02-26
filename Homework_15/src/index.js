bc44bd7acac6e29f2fb5a524c059691745b73668
Владимир Ширина <64553688+Tionthe1@users.noreply.github.com>
2023-02-26 23:58:47 +0200

    Homework_15

---
+++ Homework_15/src/index.js
@@ -0,0 +1,10 @@
+import { doAllCalculations } from "./functions";
+import { SumOfNumbers } from "./functions";
+import { genPassword } from "./functions";
+import { getRandomArray } from "./functions";
+import { getSubjects } from "./functions";
+import { student } from "./functions";
+import {} from "./functions";
+import {} from "./functions";
+import {} from "./functions";
+import {} from "./functions";
